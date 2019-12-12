d4fc3a607e25ab2f4a220d72c1c5987de69e811a
unknown <mdskrumi@gmail.com>
2019-12-12 20:14:49 +0600

    redone 3n+1

---
+++ UVA/100 - The 3n + 1 problem.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include<ctype.h>
+#include<string.h>
+
+#include<stdio.h>
+
+#define TAKING         freopen("input.txt" , "r" , stdin);
+#define MAKING         freopen("output.txt","w" , stdout);
+
+#define     ll         long long
+#define     sf         scanf
+#define     pf         printf
+#define     pb         push_back
+#define     mp         make_pair
+#define     DOS        10000000000
+#define     OO         0xfffffff
+#define     nl         "\n"
+#define     BOLT       ios_base::sync_with_stdio(0)
+
+#define     all(x)     x.begin(), x.end()
+#define     rall(x)    x.rbegin(), x.rend()
+#define     fi         first
+#define     se         second
+#define     sz(v)      (int)(v).size()
+
+using namespace std;
+
+inline int ini(){int n;cin >> n;return n;}
+inline ll inl(){ll n;cin >> n;return n;}
+inline double ind(){double n;cin >> n;return n;}
+inline string ins(){string n;cin >> n;return n;}
+inline string insl(){string n;getline(cin,n);return n;}
+
+
+inline int string_to_int(string s){int n;stringstream ss;ss << s;ss >> n;return n;}    // Same for double and long long
+inline string int_to_string(int n){string s;stringstream ss;ss << n;ss >> s;return s;} // Same for double and long long
+inline long long string_to_Long_Long(string s){ll res = 0;for(int i = 0 ; i < s.size() ; i++){res = res*10 + (s[i]-'0');}return res;}
+
+
+int cal_Ans(int n){
+    int ans = 1;
+    while(n!=1){
+        if(n%2==0){
+            n/=2;
+        }else n = 3*n + 1;
+        ans++;
+    }
+    return ans;
+}
+
+
+int main(){
+    BOLT;
+    int l , r , ans;
+    while(cin >> l >> r){
+        ans = 0;
+        for(int i = min(l,r) ; i <= max(l,r); i++){
+            ans = max(ans,cal_Ans(i));
+        }
+        cout << l << " " << r << " " << ans << nl;
+    }
+return 0;
+}
+
+
+
+
+
