cc1d091b90710469a9823592d1a9fd65f46c76fc
mdskrumi <mdskrumi@gmail.com>
2020-02-02 02:13:36 +0600

    222020

---
+++ CF/NotOne/489C - Given Length and Sum of Digits.cpp
@@ -0,0 +1,136 @@
+#include<bits/stdc++.h>
+#include<ctype.h>
+#include<bits/stdc++.h>
+#include<ctype.h>
+#include<string.h>
+
+#include<stdio.h>
+
+#define     INPUT                   freopen("input.txt" , "r" , stdin);
+#define     OUTPUT                  freopen("output.txt","w" , stdout);
+
+#define     ll                      long long
+#define     sf                      scanf
+#define     pf                      printf
+#define     pb                      push_back
+#define     mp                      make_pair
+#define     nl                      "\n"
+#define     BOLT                    ios_base::sync_with_stdio(0)
+
+#define     all(x)                  x.begin(), x.end()
+#define     rall(x)                 x.rbegin(), x.rend()
+#define     max3(a,b,c)             max(a,max(b,c))
+#define     min3(a,b,c)             min(a,min(b,c))
+#define     fi                      first
+#define     se                      second
+#define     sz(v)                   (int)(v).size()
+#define     REMOVE_ALL(str , c)     str.erase(remove(str.begin(), str.end(), c), str.end())
+#define     PIE                     acos(-1)
+#define     pline                   cout << nl
+
+using namespace std;
+
+inline string delSpaces(string &str){
+   str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+   return str;
+}
+
+
+
+inline int ini(){int n;cin >> n;return n;}
+inline int ini(string s){ cout << s; int n;cin >> n;return n;}
+
+inline ll inl(){ll n;cin >> n;return n;}
+inline ll inl(string s){ cout << s; ll n;cin >> n;return n;}
+
+inline double ind(){double n;cin >> n;return n;}
+inline double ind(string s){ cout << s; double n;cin >> n;return n;}
+
+inline string ins(){string n;cin >> n;return n;}
+inline string ins(string s){ cout << s;string n;cin >> n;return n;}
+
+inline string insl(){string n;getline(cin,n);return n;}
+inline string insl(string s){ cout << s;string n;getline(cin,n);return n;}
+
+inline int string_to_int(string s){int n;stringstream ss;ss << s;ss >> n;return n;}    // Same for double and long long
+inline string int_to_string(int n){string s;stringstream ss;ss << n;ss >> s;return s;} // Same for double and long long
+inline long long string_to_Long_Long(string s){ll res = 0;for(int i = 0 ; i < s.size() ; i++){res = res*10 + (s[i]-'0');}return res;}
+
+typedef vector <int> 	   	 vi;
+typedef pair <int,long long> 	pii;
+
+
+int main(){
+    BOLT;
+
+    int m = ini() , s = ini();
+    int arr[m];
+    int ns = s;
+
+    if(m==1 && s== 0){
+        cout << "0 0";
+        return 0;
+    }
+
+    for(int i = 0 ; i < m ; i++){
+        if(i==0){
+            arr[i] = 1;
+        }else{
+            arr[i] = 0;
+        }
+    }
+    s = s - 1;
+    if(s<0){
+        cout << -1 << " " << -1 << nl;
+    }
+    else if(s==0){
+        for(int i = 0 ; i < m ; i++){
+            cout << arr[i];
+        }
+        cout << " ";
+        for(int i = 0 ; i < m ; i++){
+            cout << arr[i];
+        }
+    }
+    else{
+        for(int i = m-1 ; i >=0 ; i--){
+            int toAdd = 9 - arr[i];
+            if(toAdd <= s){
+                s -= toAdd;
+                arr[i] += toAdd;
+            }else{
+                arr[i]+=s;
+                s=0;
+                break;
+            }
+
+        }
+        if(s!=0){
+            cout << -1 << " " << -1 << nl;
+            return 0;
+        }
+        for(int i = 0 ; i < m ; i++){
+            cout << arr[i];
+        }
+        cout << " ";
+
+        for(int i = 0 ; i < m ; i++){
+            int toadd = 9;
+            if(toadd < ns){
+                cout << 9;
+                ns-=9;
+            }else{
+                cout << ns ;
+                ns = 0;
+            }
+        }
+
+
+
+    }
+
+return 0;
+}
+
+
+
